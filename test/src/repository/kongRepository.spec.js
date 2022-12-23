db3ee4aca1037784043ec583c1ad94951f5c6288
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-12-23 06:16:29 +0100

    Update kongRepository.spec.js

--- test/src/repository/kongRepository.spec.js
+++ test/src/repository/kongRepository.spec.js
@@ -1,22 +1,33 @@
-const expect= require("chai").expect;
-const specValidator= require("../../utils/specValidator");
+const chai= require("chai");
+const kongRepository= require("../../../src/repository/kongRepository");
 const fs = require('fs');
 const yaml = require('js-yaml');
-
+var path = require('path');
+const genereratedDeckConf= require('./validDeckConf').genereratedDeckConf;
+const genereratedDeckConf2= require('./validDeckConf').genereratedDeckConf2;
+const deepEqualInAnyOrder = require('deep-equal-in-any-order');
+chai.use(deepEqualInAnyOrder);
+const { expect } = chai;
 
 describe ('Kong Repository',()=>{
     describe ('generate Deck Declarative File',()=>{
-        it('should return valid deck declarite file',() => {
-            const data = yaml.load(fs.readFileSync('C:\\dev\\nodejs\\api-catalog-service-node-2\\src\\test\\repository\\testSpec.yaml', 'utf8'));
-            const deckConfData = yaml.load(fs.readFileSync('C:\\dev\\nodejs\\api-catalog-service-node-2\\src\\test\\repository\\validDeckConf.yaml', 'utf8'));
-            const deckConfFile= generateDeckDeclarativeFile(apiName, data, backendHost)
-            expect(deckConfData).to.equal(deckConfFile);
+        it('should return valid deck declarite file',(done) => {
+            var testSpecPath = path.join(__dirname, './') + 'testSpec.yaml' ;
+            const data = yaml.load(fs.readFileSync(testSpecPath));
+            const backendHost='https://localhost';
+            apiName='testapi';
+            const deckConfFile= kongRepository.generateDeckDeclarativeFile(apiName, data, backendHost);
+            expect(deckConfFile).to.deep.equalInAnyOrder(genereratedDeckConf);
+            done();
         }),
-        it('should return validation error',() => {
-            const data = yaml.load(fs.readFileSync('C:\\dev\\nodejs\\api-catalog-service-node-2\\src\\test\\utils\\testSpec2.yaml', 'utf8'));
-            return specValidator.openAPIValidator(data).then(validationResult => {
-                expect(validationResult.length).to.equal(3)
-            })
+        it('should return valid deck declarite file',(done) => {
+            var testSpecPath = path.join(__dirname, './') + 'testSpec.yaml' ;
+            const data = yaml.load(fs.readFileSync(testSpecPath));
+            const backendHost='http://localhost';
+            apiName='testapi';
+            const deckConfFile= kongRepository.generateDeckDeclarativeFile(apiName, data, backendHost);
+            expect(deckConfFile).to.deep.equalInAnyOrder(genereratedDeckConf2);
+            done();
         })
    })
 })
