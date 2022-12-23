2c196098ec27fde049baa410945fd31682d6ccea
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-12-23 06:17:57 +0100

    Update specValidator.spec.js

--- test/src/utils/specValidator.spec.js
+++ test/src/utils/specValidator.spec.js
@@ -1,18 +1,22 @@
 const expect= require("chai").expect;
-const specValidator= require("../../utils/specValidator");
+const specValidator= require("../../../src/utils/specValidator");
 const fs = require('fs');
 const yaml = require('js-yaml');
+var path = require('path');
+
 
 
 describe ('utils spec Validator',()=>{
     it('should return no validation error',() => {
-        const data = yaml.load(fs.readFileSync('C:\\dev\\nodejs\\api-catalog-service-node-2\\src\\test\\utils\\testSpec.yaml', 'utf8'));
+        var validfilePath = path.join(__dirname, './') + 'testSpec.yaml' ;
+        const data = yaml.load(fs.readFileSync(validfilePath));
         return specValidator.openAPIValidator(data).then(validationResult => {
             expect(validationResult.length).to.equal(0)
         })
     }),
     it('should return validation error',() => {
-        const data = yaml.load(fs.readFileSync('C:\\dev\\nodejs\\api-catalog-service-node-2\\src\\test\\utils\\testSpec2.yaml', 'utf8'));
+        var invalidfilePath = path.join(__dirname, './') + 'testSpec2.yaml' ;
+        const data = yaml.load(fs.readFileSync(invalidfilePath));
         return specValidator.openAPIValidator(data).then(validationResult => {
             expect(validationResult.length).to.equal(3)
         })
