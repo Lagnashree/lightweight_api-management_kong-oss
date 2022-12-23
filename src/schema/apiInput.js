35112ad2e89924d8fce1ff01f29cfb9d9f74a55a
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-12-23 06:21:40 +0100

    Update apiInput.js

--- src/schema/apiInput.js
+++ src/schema/apiInput.js
@@ -31,7 +31,7 @@ let schema= {
             "type": "string",
             "required": true,
             "minLength": 15,
-            //"pattern": "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+            "pattern": "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
         },
         "specUrl": {
             "type": "string",
