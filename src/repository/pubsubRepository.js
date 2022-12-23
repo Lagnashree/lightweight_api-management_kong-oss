c24dcd5b2cbd88d1e04a4a9d06b9d0b5cfe6fd4a
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-12-23 06:21:09 +0100

    Update pubsubRepository.js

--- src/repository/pubsubRepository.js
+++ src/repository/pubsubRepository.js
@@ -1,17 +1,14 @@
 const path = require("path");
+const dotenv = require("dotenv");
 const { PubSub } = require("@google-cloud/pubsub");
-const topicName = 'api-catalog-topic';
-
+const { getSecret } = require("../../conf/secretManager.js");
+const cloudSecret = getSecret();
+const topicName = cloudSecret.PUBSUB_TOPIC;
+const envFilePath = path.join(__dirname, "../../", ".env");
+dotenv.config({ path: envFilePath });
 exports.postEvent = async function (eventData) {
     const projectId = process.env.PROJECT_ID;
-    let serviceKey = "";
-
-    //if (process.env.ENVIRONMENT === "LOCAL") {
-    serviceKey = path.join(__dirname, "..", "/service-account.json");
-    //}
-
-
-    let pubsub = new PubSub({ projectId: projectId, keyFilename: serviceKey });
+    let pubsub = new PubSub({ projectId: projectId });
 
     try {
         const data = JSON.stringify(eventData);
