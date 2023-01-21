9667303860941cbb670cd37244ad11b9d9c93e8e
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2023-01-22 00:38:09 +0100

    Update getSpecService.js

--- src/service/getSpecService.js
+++ src/service/getSpecService.js
@@ -6,18 +6,13 @@ const { BaseError, notFound, internalServerError, serviceUnavailable, badRequest
 
 exports.getSpecInfo = async function getSpecInfo (apiName, apiVersion, environment, uniqueRqId) {
     try {
-       
-        logger.log('info',`reqId: ${uniqueRqId}. Info about get API spec file service`);
-
-        
-        
         try {
             logger.log('info',`reqId: ${uniqueRqId}. Spec file is downloaded from gcs bucket`);
             return await gcsRepositiory.downloadFile(apiName, apiVersion, environment);
         }
         catch (error) {
             logger.log('error',`reqId: ${uniqueRqId},  Error while downloading spec file from GCS bucket,  ${error}`);
-            throw new BaseError(internalServerError, `unable to download spec file from GCS for API ${reqBody.apiName}`)
+            throw new BaseError(internalServerError, `unable to download spec file from GCS for API ${apiName}`)
         }    
     }
 
