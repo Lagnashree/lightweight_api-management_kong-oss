4c43044f2bffdc22266bea1df748e0c7afc751b2
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2023-01-22 00:37:18 +0100

    Update requestValidator.js

--- src/utils/requestValidator.js
+++ src/utils/requestValidator.js
@@ -7,7 +7,14 @@ function apiPostReqValidator(reqBody) {
     return v.validate(reqBody, apiInputSchema.schema);
 }
 function apiGetReqValidator() {
-    
+    let validationResult={errors:[]};
+    if (typeof req.params.apiName == 'undefined' || req.params.apiName == '' || typeof req.params.apiVersion == 'undefined' || req.params.apiVersion == '' || typeof req.params.environment == 'undefined' || req.params.environment == '') {
+        validationResult.errors.push("apiName, apiVersionn and environment are manadatory field");
+    }
+    if (req.params.environment && !['prod', 'ppe', 'dev'].includes(req.params.environment.toLowerCase())) {
+        validationResult.errors.push("Invalid environment field. Value should be one of dev, ppe, prod");
+    }
+    return validationResult;
 }
 function apiDeleteReqValidator(req) {
     let validationResult={errors:[]};
@@ -17,9 +24,22 @@ function apiDeleteReqValidator(req) {
     if (req.params.environment && !['prod', 'ppe', 'dev'].includes(req.params.environment.toLowerCase())) {
         validationResult.errors.push("Invalid environment field. Value should be one of dev, ppe, prod");
     }
+    return validationResult;
+}
+function getSpecReqValidator(req){
+    console.log('here');
+    let validationResult={errors:[]};
+    if (typeof req.params.apiName == 'undefined' || req.params.apiName == '' || typeof req.params.apiVersion == 'undefined' || req.params.apiVersion == '' || typeof req.params.environment == 'undefined' || req.params.environment == '') {
+        validationResult.errors.push("apiName, apiVersionn and environment are manadatory field");
+    }
+    if (req.params.environment && !['prod', 'ppe', 'dev'].includes(req.params.environment.toLowerCase())) {
+        validationResult.errors.push("Invalid environment field. Value should be one of dev, ppe, prod");
+    }
+    return validationResult;
 }
 module.exports = {
     apiPostReqValidator,
     apiGetReqValidator,
-    apiDeleteReqValidator
+    apiDeleteReqValidator,
+    getSpecReqValidator
 }
